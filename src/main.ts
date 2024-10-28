a63e248aa23d8b41c225bbfc11985818ef596511
21120447 <haonhat2729@gmail.com>
2024-10-28 09:45:00 +0700

    chore: add cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: '*',
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());
